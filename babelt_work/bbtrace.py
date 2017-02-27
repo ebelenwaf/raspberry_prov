c773cb572cbe68549599c8e9fac7fe3e85e21f91
Andre Campbell <campbellandre97@gmail.com>
2017-02-27 08:22:21 -0500

    bbtrace: outputs the keys and their contents for each sensor read in. Writes its output to a text file

--- babelt_work/bbtrace.py
+++ babelt_work/bbtrace.py
@@ -10,5 +10,13 @@ trace_collection = babeltrace.TraceCollection()
 
 trace_collection.add_trace(trace_path, 'ctf')
 #Outputs the event keys of each trace that was recorded.
+results = open('output.txt', 'w')
+
 for event in trace_collection.events:
-    print(', '.join(event.keys()))
+    #print(', '.join(event.keys()))
+    #results.write(', '.join(event.keys()))
+    for key in event.keys():
+        print(('%s : %s ,' %(key, event[key])))
+        results.write(('%s : %s ,' %(key, event[key])))
+    print('\n')
+    results.write('\n')
