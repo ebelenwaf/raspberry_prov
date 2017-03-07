19ef8de8ed488058f87b6ea474bc64d0d3ee2beb
Andre Campbell <drecampbell97@gmail.com>
2017-03-07 11:54:05 -0500

    babeltrace: Modified bbtrace.py to create prov document

--- babelt_work/bbtrace.py
+++ babelt_work/bbtrace.py
@@ -3,41 +3,28 @@ import babeltrace
 import sys
 import re
 from prov.dot import prov_to_dot
-from prov.model import ProvDocument
+from prov.model import ProvDocument, Namespace, Literal, PROV, Identifier
 
 
 # get the trace path from the first command line argument
 trace_path = sys.argv[1]
-
 trace_collection = babeltrace.TraceCollection()
-
 trace_collection.add_trace(trace_path, 'ctf')
 #Outputs the event keys of each trace that was recorded.
 def ctfToProv():
     d1 = ProvDocument()
     ex = Namespace('ex', 'http://example/')  # namespaces do not need to be explicitly added to a document
-#   results = open('output.txt', 'w')
-    agents = ['SensorOne', 'SensorTwo', 'SensorThree']
-    data = ['timestamp', 'humidity', 'temperature']
+    data = ['timestamp', 'humidity', 'temperature', 'sensor_info', 'device_info']
     for event in trace_collection.events:
 
-        for data in event.keys():
-
-            print(('%s : %s ,' %(key, event[key])))
-#           results.write(('%s : %s ,' %(key, event[key])))
-        print('\n')
-#	with open('../ctf-output.txt', 'r') as file_object:
-#		readFile(file_object)
-#
-#	d1.serialize(indent=2)
-#	d1.serialize('article-prov.json')
-#	dot = prov_to_dot(d1)
-#	dot.write_png('article-prov.png')
-#
-#	attributes['prov:TimeStamp'] = timestamp
-#	l = ['senread:me', 'senread:you', 'senread:us', 'senread:who']
-#	e1 = prov_dm.entity(l[c], attributes)
-#	c = (c+1)% len(l)
-#	prov_dm.wasAttributedTo(e1, tags[c])
-#	#prov_dm.entity(attributes)
-ctfToProv()
+        dataset = {'ex:'+data[0]:event[data[0]], 'ex:'+data[1]:event[data[1]], 'ex:'+data[2]:event[data[2]]}
+        e1 = d1.entity(ex['dataset'],dataset)
+        sensor_agent = d1.agent('ex:'+event['sensor_info'])
+        device_agent = d1.agent('ex:'+event['device_info'])
+        activity = d1.activity('ex:read')
+        d1.wasGeneratedBy(e1, activity)
+        d1.wasAssociatedWith(activity,sensor_agent)
+        d1.used(sensor_agent, device_agent)
+        d1.serialize('hey.json')
+    return d1
+prov_document = ctfToProv()
