a813c4d992eab0417f6347096d552a88f22a97da
System Administrator <root@Ebelechukwus-MacBook-Pro.local>
2017-03-07 17:39:55 -0500

    update

--- babelt_work/bbtrace.py
+++ babelt_work/bbtrace.py
@@ -9,6 +9,8 @@ trace_path = sys.argv[1]
 trace_collection = babeltrace.TraceCollection()
 
 trace_collection.add_trace(trace_path, 'ctf')
+if col.add_trace(sys.argv[1], 'ctf') is None:
+    raise RuntimeError('Cannot add trace')
 #Outputs the event keys of each trace that was recorded.
 results = open('output.txt', 'w')
 
