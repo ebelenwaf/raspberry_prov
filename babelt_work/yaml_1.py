ca46f1b883d7ade1160ef7efeed220b93c53df2c
Ebelechukwu Nwafor <ebelechukwu@Ebelechukwus-MacBook-Pro.local>
2017-09-28 16:20:12 -0400

    updates

---
+++ babelt_work/yaml_1.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+import yaml
+
+
+def event_field(config_file):
+
+	with open(config_file, 'r') as stream:
+	    try:
+	        list_doc = yaml.load(stream)
+
+	        value = list_doc["metadata"]
+
+	        val = value['streams']
+
+	        default = val['default']
+
+	        events = default['events']
+	        sensor_readings = events['sensor_readings']
+
+
+	        payload_type = sensor_readings['payload-type']
+
+	        fields = payload_type['fields']
+
+	        event_value = fields.keys()
+
+
+	        #for event in event_value:
+	        	#print(event)
+
+	    except yaml.YAMLError as exc:
+	        print(exc)
+
+
+return event
