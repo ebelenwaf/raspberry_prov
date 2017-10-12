f4b6e4dad810885f994c993516a3dd06f2f6285b
Gedare Bloom <gedare@rtems.org>
2017-10-12 13:39:09 -0400

    thermostat: refactor temperature reading

--- samples/temperature/thermostat.c
+++ samples/temperature/thermostat.c
@@ -9,25 +9,46 @@
 #include <barectf.h>
 #include <sys/stat.h>
 
-int main(void)
+struct TH {
+  float temperature;
+  float humidity;
+};
+
+/**
+ * Attempts to get a valid temperature and humidity reading up to tries times.
+ * If successful, puts the valid reading into th and returns 0.
+ * If unsuccessful, puts 0 in th and returns non-zero.
+*/
+int get_temperature_and_humidity(struct TH* th, int tries)
 {
   const int sensor = 22;
   const int pin_number = 4;
-  float humidity = 0.0f, temperature = 0.0f;
+  int result = -1, i;
+  th->temperature = th->humidity = 0.0f;
+  for (i = 0; i < tries; i++) {
+    result = pi_2_dht_read(sensor, pin_number, &th->humidity, &th->temperature);
+    if (!result) break;
+  }
+  return result;
+}
+
+int main(void)
+{
   int hum = 0, temp = 0, i;
   struct barectf_platform_linux_fs_ctx *platform_ctx;
+  struct TH th;
 
   /* initialize platform */
   platform_ctx = barectf_platform_linux_fs_init(1024, "ctf", 1, 2, 20);
 
   for (i = 0; i < 10; i++) {
-    int result = pi_2_dht_read(sensor, pin_number, &humidity, &temperature);
+    int result = get_temperature_and_humidity(&th, 5);
+    printf("result = %d\n", result);
+    printf("H = %f%, T = %f *C\n", th.humidity, th.temperature);
 
-    printf( "result = %d\n", result);
-    printf( "Humidity = %f%, Temperature = %f*C \n", humidity, temperature);
-
-    temp = temperature;
-    hum = humidity;
+    temp = th.temperature;
+    hum = th.humidity;
+    printf("h = %d%, t = %d *C\n", hum, temp);
     barectf_default_trace_sensor_events(
         barectf_platform_linux_fs_get_barectf_ctx(platform_ctx), temp, hum,
         "device_1", "DHT_22_1", "read");
