1a3c7c026fe2fda02fe45202953eee5f18f34297
Gedare Bloom <gedare@rtems.org>
2018-03-30 09:30:56 -0400

    injection.py: fix arg processing and return values from injectMsg

--- J1939_experiments/injection.py
+++ J1939_experiments/injection.py
@@ -8,14 +8,16 @@ import numpy as np
 
 
 def usage():
-   print "Usage: injection.py -[hvi:o:d:n:f:p:]\n\
+   print "Usage: injection.py -[hvi:o:d:p:n]\n\
  -h --help         print this help\n\
  -v --verbose      print more information [False]\n\
  -i --input        input log filename [driving_data.csv]\n\
- -o --output       output log filename [injection_driving_data.csv]\n\
- -d --disregard    denominator of 1/d % events not subject to injection [2]\n\
+ -o --output       output log filename [anomalous_data.csv]\n\
+ -d --disregard    denominator of 1/d % events not subject to injection [4]\n\
 					   Note: d must match that used during injection, and\n\
 					   the max window size is n/d.\n\
+-p --injection-point [0] \n\
+-n --num-injections [10] \n\
 "
 
 
@@ -54,14 +56,14 @@ def injectMsg(in_file, disregard, gap_to_inject ,mal_str, num_msg):
 		num_msg - The number of messages desired for injection.
 
 	"""
-	inject = True
-
-	if gap_to_inject <= 0:
-		inject = False
 
 	anom_data = []
 
 	data = read_lists_from_CSV(in_file)
+
+	if gap_to_inject <= 0:
+		return -1, data
+
 	timestamp_rows = [ HHMMSSmmuu_ts_to_microseconds(row[0]) for row in data]
 	start_idx = int((1.0/disregard) * len(data))
 
@@ -78,8 +80,7 @@ def injectMsg(in_file, disregard, gap_to_inject ,mal_str, num_msg):
 			desired_gaps_idx.append(event_idx)
 
 
-	injected_idx = None
-	inject_failed = False
+	injected_idx = -1
 
 	for x in range(len(data)-1):
 
@@ -88,7 +89,7 @@ def injectMsg(in_file, disregard, gap_to_inject ,mal_str, num_msg):
 		
 		elif x > start_idx:
 			try:
-				if x == desired_gaps_idx[gap_to_inject] and inject == True:
+				if x == desired_gaps_idx[gap_to_inject]:
 
 					curr_ts = timestamp_rows[x]
 					injected_idx = x + 1
@@ -101,25 +102,25 @@ def injectMsg(in_file, disregard, gap_to_inject ,mal_str, num_msg):
 					anom_data.append(data[x])
 
 			except:
-				inject_failed = True
 				print "ERROR: Cannot inject message. gap_to_inject is either out of range."
-				return -1
+				return -1, data
 
 		else:
 			anom_data.append(data[x])
 	
 	
-	return anom_data, injected_idx
+	return injected_idx, anom_data
 
 def main():
 
 	# Default Parameters
 	in_file = 'driving_data.csv'
-	disregard = 2
-	inject_point = 1
+	disregard = 4
+	inject_point = 0
 	malicious_msg = '1,2,C000003x,C000003x,CAN - EXT,8,01 41 A0 FF FF FF FF FF,Tx'
 	output = 'anomalous_data.csv'
 	number_injections = 10
+	verbose = False
 
 
 	
@@ -128,7 +129,7 @@ def main():
 	try:
 		opts, args = getopt.getopt(sys.argv[1:], "hvi:o:d:p:n",
 			["help", "verbose", "input=", "output=",
-			 "disregard="])
+			 "disregard=", "injection-point=", "num-injections="])
 	
 	except getopt.GetoptError, err:
 		print(str(err))
@@ -148,20 +149,20 @@ def main():
 		elif opt in ("-d", "--disregard"):
 			disregard = int(arg)
 		elif opt in ("-p", "--inject_point"):
-			numevts = int(arg)
+			inject_point = int(arg)
 		elif opt in ("-n", "--number_injections"):
-			fraction = int(arg)
+			number_injections = int(arg)
 		else:
 			print("Unhandled option: " + opt + "\n")
 			usage()
 			sys.exit(2)
 
-	malData = injectMsg(in_file, disregard, inject_point, malicious_msg, number_injections )
-	write_lists_to_CSV(output, malData[0])
+	index, malData = injectMsg(in_file, disregard, inject_point, malicious_msg, number_injections )
+	if index >= 0:
+		write_lists_to_CSV(output, malData)
 
 	if verbose == True:
-
-		print " Messages injected at index %d " % malData[1]
+		print " Messages injected at index %d " % index
 
 
 
