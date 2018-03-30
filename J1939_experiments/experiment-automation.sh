f96eadd98b8cb5f591327f120e3b3ceeace7ae21
Gedare Bloom <gedare@rtems.org>
2018-03-30 16:38:19 -0400

    experiment-automation.sh: clean up and make it almost ready to go

--- J1939_experiments/experiment-automation.sh
+++ J1939_experiments/experiment-automation.sh
@@ -6,35 +6,17 @@ if [[ ! -d ${OUTDIR} ]]; then
   mkdir ${OUTDIR}
 fi
 
-#if [[ -f ${OUTDIR}/${FILE} ]]; then
-#  echo "remove previous sim_res.txt"
-#  exit 1
-#fi
-#touch ${OUTDIR}/${FILE}
-
-## Variables are:
-##  tasks
-##  utilization (and distribution)
-##  pq size (min/max)
-##  pq ops (min/max)
-
-## FIXME: use more trials
-#TRIALS=100000
-TRIALS=10000
-
-## FIXME: vary the util distribution
-#for dist in 1 2 3
-
-
-  for pruning in 0 1 2; do
-    OUTDIR=out/${pruning}/
-    mkdir ${OUTPUT}
-    for f in 1.0 0.5 0.25 0.125; do
-      FILE=experiment_data_${f}.txt
-      for x in {0..2}; do
-        python experiment.py -i log/anomalous_data_${0}.csv -f ${f} -d 4 ... >> ${OUTDIR}/${FILE}
-
-      done
+for pruning in 0 1 2; do
+  OUTDIR=out/${pruning}/
+  mkdir ${OUTDIR}
+  for f in 1.0 0.5 0.25 0.125 0.0625 0.03125 0.015625 0.0078125 0.00390625; do
+    FILE=experiment_data_${f}.txt
+    echo -n "" > ${OUTDIR}/${FILE}
+    for infile in `ls log/*`; do
+      echo "python experiment.py -i ${infile} -f ${f} -d 4 -p ${pruning} >> ${OUTDIR}/${FILE}"
+      # TODO: Uncomment this when ready to run it for real!
+      #python experiment.py -i ${infile} -f ${f} -d 4 -p ${pruning} >> ${OUTDIR}/${FILE}
     done
   done
+done
 
