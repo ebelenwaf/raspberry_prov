a04604b42bf6f69641816801fa1eb9033d33ea80
Gedare Bloom <gedare@rtems.org>
2018-04-01 23:48:03 -0400

    experiment-automation.sh: reduce number of experiments to about 28 per inner

--- J1939_experiments/experiment-automation.sh
+++ J1939_experiments/experiment-automation.sh
@@ -9,10 +9,11 @@ fi
 for pruning in 0 1 2; do
   OUTDIR=out/${pruning}/
   mkdir ${OUTDIR}
-  for f in 1.0 0.5 0.25 0.125 0.0625 0.03125 0.015625 0.0078125 0.00390625; do
+  #for f in 1.0 0.5 0.25 0.125 0.0625 0.03125 0.015625 0.0078125 0.00390625; do
+  for f in 1.0 0.0625 0.00390625; do
     FILE=experiment_data_${f}.txt
     echo -n "" > ${OUTDIR}/${FILE}
-    for infile in `ls log/*`; do
+    for infile in `ls log/driving_data_*00_*`; do
       echo "python3.5 experiment.py -i ${infile} -f ${f} -d 4 -p ${pruning} >> ${OUTDIR}/${FILE}"
       # TODO: Uncomment this when ready to run it for real!
       #python3.5 experiment.py -i ${infile} -f ${f} -d 4 -p ${pruning} >> ${OUTDIR}/${FILE}
