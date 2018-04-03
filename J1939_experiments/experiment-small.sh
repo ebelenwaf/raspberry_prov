5f4e34c5e28c761926c9ed9c10d5877bdb7f271f
Gedare Bloom <gedare@rtems.org>
2018-04-02 21:27:24 -0400

    Add experiment-small.sh

---
+++ J1939_experiments/experiment-small.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+OUTDIR=out
+
+if [[ ! -d ${OUTDIR} ]]; then
+  mkdir ${OUTDIR}
+fi
+
+for pruning in 0 ; do
+  OUTDIR=out/${pruning}/
+  mkdir ${OUTDIR}
+  #for f in 1.0 0.5 0.25 0.125 0.0625 0.03125 0.015625 0.0078125 0.00390625; do
+  for f in 1.0 0.5 0.25; do
+    mkdir ${OUTDIR}/${f}
+    FILE=experiment_data_${f}.txt
+    echo -n "" > ${OUTDIR}/${FILE}
+    for infile in `ls small/driving_data_*0_*` ; do
+      echo "python3 experiment.py -i ${infile} -f ${f} -d 4 -p ${pruning} >> ${OUTDIR}/${FILE}"
+      # TODO: Uncomment this when ready to run it for real!
+      #python3.5 experiment.py -i ${infile} -f ${f} -d 4 -p ${pruning} >> ${OUTDIR}/${FILE}
+    done
+  done
+done
+
